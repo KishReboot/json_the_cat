e39ae793ea25c3354359d3ebc4eefa0be8012a64
KishReboot <russellsalopek@pm.me>
2022-11-24 18:04:26 -0500

    Added an index file to handle running the search function, added a test folder with a file to handle our tests using mocha.

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const catBreed = process.argv[2];
+
+fetchBreedDescription(catBreed, (error, desc) => {
+  
+  if (error) console.log('Error fetch details:', error);
+  else console.log(desc);
+  
+});
