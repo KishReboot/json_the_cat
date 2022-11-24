eea382e71061c08d68698ec4367a2f6a2171d8bd
KishReboot <russellsalopek@pm.me>
2022-11-24 17:48:21 -0500

    Handles errors when URL has a typo/wrong, and when the breed name is wrong

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,17 @@
-/* Load https://api.thecatapi.com/v1/images/search
- * Get the first Array object of the JSON response
- * Load it’s .url
- * Enjoy the kitty 
- */
 const request = require('request');
 
-// Allow the user to specify the breed name using command-line arguments.
 const catBreed = process.argv[2];
-const catURL = `https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`;
+const catURL = `https://api.thecataps.com/v1/breeds/search?q=${catBreed}`;
 
 request(catURL, (error, response, body) => {
 
-const data = JSON.parse(body);
-console.log(data);
-console.log(typeof data);
+  if (error) return console.log(`The request failed: ${error}`);
+  
+  const data = JSON.parse(body);
+  
+  const breed = data[0];
+
+  if (breed) console.log(breed.description);
+  else console.log(`Are you sure ${catBreed} is correct?`);
 
 });
